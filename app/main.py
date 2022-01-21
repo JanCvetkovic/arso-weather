27c53b45d5fbfd999f4e344171ed5a9e9293c7e1
JanCvetkovic <cvetkovic.jan@gmail.com>
2022-01-21 23:00:52 +0100

    added moisture and altitude

--- app/main.py
+++ app/main.py
@@ -13,6 +13,10 @@ entries = arso.find_all("metData")
 for metData in entries:
     title = metData.domain_longTitle.text
     temperature = metData.t.text
-    print(f" City: {title} \n Temperature: {temperature}°C \n --------------------")
-
-   
+    description = metData.nn_var_desc.text
+    moisture = metData.rh.text
+    altitude = float(metData.domain_altitude.text)
+    altitude = round(altitude)
+    print(
+        f" City: {title} ({altitude}m) \n Description: {description}.\n Temperature: {temperature}°C \n Moisture: {moisture}%\n --------------------"
+    )
