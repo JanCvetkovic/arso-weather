164d8d7ddeba73efa251ce44e932e5cd797f1482
JanCvetkovic <cvetkovic.jan@gmail.com>
2022-01-22 11:06:52 +0100

    removed description

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ url = requests.get(
     "http://www.meteo.si/uploads/probase/www/observ/surface/text/sl/observationAms_si_latest.xml"
 )
 
-print(url)
 arso = BeautifulSoup(url.content, "xml")
 
 entries = arso.find_all("metData")
@@ -13,10 +12,10 @@ entries = arso.find_all("metData")
 for metData in entries:
     title = metData.domain_longTitle.text
     temperature = metData.t.text
-    description = metData.nn_var_desc.text
+
     moisture = metData.rh.text
     altitude = float(metData.domain_altitude.text)
     altitude = round(altitude)
     print(
-        f" City: {title} ({altitude}m) \n Description: {description}.\n Temperature: {temperature}°C \n Moisture: {moisture}%\n --------------------"
+        f" City: {title} ({altitude}m) \n Temperature: {temperature}°C \n Moisture: {moisture}%\n --------------------"
     )
